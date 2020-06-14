0bc6085967804dd6378a51050f99823d367744f8
cleberson-dev <cleberson.dev@gmail.com>
2020-06-14 09:58:27 -0300

    Commit inicial

---
+++ src/test/java/com/clebersondev/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clebersondev.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
