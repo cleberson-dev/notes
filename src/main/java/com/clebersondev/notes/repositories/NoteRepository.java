35e81b2ecbcb11dc731a44b608de520065834518
cleberson-dev <cleberson.dev@gmail.com>
2020-06-15 11:25:31 -0300

    Criado repositórios para persistência

--- src/main/java/com/clebersondev/notes/repositories/NoteRepository.java
+++ src/main/java/com/clebersondev/notes/repositories/NoteRepository.java
@@ -1,15 +1,18 @@
 package com.clebersondev.notes.repositories;
 
 import com.clebersondev.notes.models.Note;
-import com.clebersondev.notes.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
     Optional<Note> findById(Long id);
 
-    Optional<Note> findByAuthor(User author);
+    @Query("SELECT n FROM Note n WHERE n.author.id = :userId")
+    List<Note> findByUserId(@Param("userId") Long userId);
 }
