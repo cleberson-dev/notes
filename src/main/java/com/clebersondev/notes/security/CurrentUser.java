e9bb6c53f1b98544d6ebd5c0b998165977001cfc
cleberson-dev <cleberson.dev@gmail.com>
2020-06-15 11:27:17 -0300

    Configurado autenticação e autorização de recursos com JWT, alcançando segurança.

---
+++ src/main/java/com/clebersondev/notes/security/CurrentUser.java
@@ -0,0 +1,12 @@
+package com.clebersondev.notes.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import java.lang.annotation.*;
+
+
+
+@Target({ ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser { }
